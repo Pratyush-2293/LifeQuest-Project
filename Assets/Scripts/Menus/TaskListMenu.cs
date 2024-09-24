f1d00a39bd5daea00f8bfd73b22551804d117fa9
Pratyush-2293 <pratyush12gawali@gmail.com>
2024-09-24 19:33:16 +0530

    Created Layout for Task Creation Menu

--- Assets/Scripts/Menus/TaskListMenu.cs
+++ Assets/Scripts/Menus/TaskListMenu.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class TaskListMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject createTaskMenu = null;
+
+    public void OnCreateNewTaskButton()
     {
-        
+        createTaskMenu.gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnCreateTaskCancelButton()
     {
-        
+        createTaskMenu.gameObject.SetActive(false);
     }
 }
