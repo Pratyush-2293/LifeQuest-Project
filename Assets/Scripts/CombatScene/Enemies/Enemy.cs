5949386227ce1de776a0c5a0270ca7684b7435d5
Pratyush-2293 <pratyush12gawali@gmail.com>
2024-10-24 17:32:05 +0530

    Fixed UI Scaling Issue in Combat Scene

--- Assets/Scripts/CombatScene/Enemies/Enemy.cs
+++ Assets/Scripts/CombatScene/Enemies/Enemy.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider healthBar;
+
+    public int health = 100;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthBar.value = health;
     }
 }
